0f30cbc66d52a97b2563ae7995ad9749acbf4272
Steven <t7seven7t@gmail.com>
2012-10-29 16:29:12 +1000

    Revert "Update progress so far."
    This reverts commit 2b0054110264db1acd3f8ac0dbf32623d5576a58.

--- src/com/minesworn/autocraft/core/ISPlugin.java
+++
@@ -1,8 +0,0 @@
-package com.minesworn.autocraft.core;
-
-public interface ISPlugin {
-	
-	public void onEnable();
-	public void onDisable();
-
-}
--- src/com/minesworn/autocraft/ships/TurnDirection.java
+++
@@ -1,5 +0,0 @@
-package com.minesworn.autocraft.ships;
-
-public enum TurnDirection {
-	RIGHT, LEFT;
-}
