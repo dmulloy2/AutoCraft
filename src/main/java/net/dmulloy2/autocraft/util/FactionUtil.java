2568e97e7e0b1597a73043c375afd7f16ee87d09
Dan Mulloy <dmulloy2@live.com>
2014-03-28 22:16:20 -0400

    Surround canPlayerUseWeapon in FactionUtil with try-catch

--- src/main/java/net/dmulloy2/autocraft/util/FactionUtil.java
+++ src/main/java/net/dmulloy2/autocraft/util/FactionUtil.java
@@ -18,29 +18,32 @@ import com.massivecraft.factions.types.Relation;
 public class FactionUtil {
 
 	public static boolean canPlayerUseWeapon(Player player) {
-		PluginManager pm = Bukkit.getPluginManager();
-		FLocation loc = new FLocation(player.getLocation());
-		FPlayer fme = FPlayers.i.get(player);
-		Faction myFaction = fme.getFaction();
-		Faction them = Board.getFactionAt(loc);
-		if (pm.isPluginEnabled("SwornNations"))
-			them = Board.getAbsoluteFactionAt(loc);
+		try {
+			PluginManager pm = Bukkit.getPluginManager();
+			FLocation loc = new FLocation(player.getLocation());
+			FPlayer fme = FPlayers.i.get(player);
+			Faction myFaction = fme.getFaction();
+			Faction them = Board.getFactionAt(loc);
+			if (pm.isPluginEnabled("SwornNations"))
+				them = Board.getAbsoluteFactionAt(loc);
 
-		// Allow in Wilderness
-		if (them.isNone())
-			return true;
+			// Allow in Wilderness
+			if (them.isNone())
+				return true;
 
-		// Deny in war/safe zones
-		if (! them.isNormal()) {
-			fme.msg("<i>You cannot use weapons in %s.", them.getTag(fme));
-			return false;
-		}
+			// Deny in war/safe zones
+			if (! them.isNormal()) {
+				fme.msg("<i>You cannot use weapons in %s.", them.getTag(fme));
+				return false;
+			}
 
-		Relation rel = myFaction.getRelationTo(them);
-		// Deny if allies
-		if (rel.isAtLeast(Relation.ALLY)) {
-			fme.msg("<i>You cannot do that in the territory of %s.", them.getTag(fme));
-			return false;
+			Relation rel = myFaction.getRelationTo(them);
+			// Deny if allies
+			if (rel.isAtLeast(Relation.ALLY)) {
+				fme.msg("<i>You cannot do that in the territory of %s.", them.getTag(fme));
+				return false;
+			}
+		} catch (Exception e) {
 		}
 
 		return true;
